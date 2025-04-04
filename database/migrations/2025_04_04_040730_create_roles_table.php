be30716a11f0c27c68e016b26b131c31f2121433
rufa-tech <rufamaedaniel89@gmail.com>
2025-04-04 12:43:14 +0800

    added_roles_and_foreign_key

--- database/migrations/2025_04_04_040730_create_roles_table.php
+++ database/migrations/2025_04_04_040730_create_roles_table.php
@@ -13,15 +13,12 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('roles', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->unsignedBigInteger('role_id');
-            $table->string('password');
-            $table->rememberToken();
+            $table->string('role_name')->unique();
+            $table->string('url');
             $table->timestamps();
+
         });
     }
 
@@ -32,6 +29,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('roles');
     }
 };
